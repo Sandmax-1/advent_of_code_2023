91b6dfa4eb55c8a33915ce92e22f296c21de3d57
Sandmax-1 <m.sandman@hotmail.co.uk>
2023-12-05 15:21:29 +0000

    add day 5 part 2

--- day_5/solution.py
+++ day_5/solution.py
@@ -1,17 +1,17 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Dict
+from typing import Dict, List, Tuple
 
 
 @dataclass()
 class MapRange:
-    name_source: str
-    name_destination: str
-    source_start: int
     destination_start: int
-    range_length: int
+    diff: int
     source_range: range
+
+    def __repr__(self) -> str:
+        return f"source_range={self.source_range}, diff={self.diff}, dest_start={self.destination_start}"  # noqa: E501
 
 
 MAP_NAMES = {
@@ -25,60 +25,94 @@ MAP_NAMES = {
 }
 
 
-def parse_input(file_name: str) -> Any:
-    maps: Dict[Any, Any] = {}
+def parse_input(file_name: str) -> Tuple[Dict[str, List[MapRange]], List[int]]:
+    maps: Dict[str, List[MapRange]] = {}
     with open(Path(os.getcwd()) / "day_5" / file_name) as f:
         for line in f.readlines():
             if line[0:5] == "seeds":
                 seeds = map(int, line.split(":")[1].strip().split(" "))
             elif "map:" in line:
                 map_name = line.split("map")[0].strip()
-                name_source = map_name.split("-to-")[0]
-                name_destination = map_name.split("-to-")[1]
-
             elif line == "\n":
                 continue
             else:
                 values = line.strip().split()
                 maps[map_name] = maps.get(map_name, []) + [
                     MapRange(
-                        name_source,
-                        name_destination,
-                        source_start=int(values[1]),
                         destination_start=int(values[0]),
-                        range_length=int(values[2]),
+                        diff=int(values[1]) - int(values[0]),
                         source_range=range(
                             int(values[1]),
                             int(values[1]) + int(values[2]),
                         ),
                     )
                 ]
-    return maps, seeds
+    return maps, list(seeds)
 
 
-# def make_maps(maps):
-#     for key, map in maps.items():
+def get_range_intersection(r1: range, r2: range) -> range:
+    r = range(max(r1.start, r2.start), min(r1.stop, r2.stop))
+    return r
+
+
+def get_non_intersecting_ranges(r1: range, r2: range) -> List[range]:
+    if r1.start < r2.start <= r2.stop < r1.stop:  # contained
+        poss_ranges = [range(r1.start, r2.start), range(r2.stop, r1.stop)]
+    elif r1.start <= r2.start <= r1.stop < r2.stop:  # overalps left side
+        poss_ranges = [range(r1.start, r2.start)]
+    elif r2.start <= r1.start <= r2.stop < r1.stop:  # overlaps right side
+        poss_ranges = [range(r2.stop, r1.stop)]
+    elif (
+        r1.start < r1.stop < r2.start < r2.stop
+        or r1.stop >= r1.start > r2.stop >= r2.start  # noqa: W503
+    ):  # disjoint
+        poss_ranges = [r1]
+    else:  # completely contained
+        poss_ranges = []
+    return poss_ranges
+
+
+def get_locations_from_seed_ranges(
+    maps: Dict[str, List[MapRange]], seed_ranges: List[range]
+) -> List[range]:
+    map_name = "seed-to-soil"
+    intersections: List[range] = []
+    disjoint_ranges = seed_ranges
+    while map_name:
+        current_map = maps[map_name]
+        if intersections:
+            disjoint_ranges = intersections + disjoint_ranges
+        intersections = []
+        for map_range in current_map:
+            intersections += [
+                range(a.start - map_range.diff, a.stop - map_range.diff)
+                for inter in disjoint_ranges
+                if (a := get_range_intersection(inter, map_range.source_range))
+            ]
+            disjoint_range_lists = [
+                get_non_intersecting_ranges(inter, map_range.source_range)
+                for inter in disjoint_ranges
+            ]
+            disjoint_ranges = [
+                item for sublist in disjoint_range_lists for item in sublist
+            ]
+
+        map_name = MAP_NAMES[map_name]
+    return intersections + disjoint_ranges
 
 
 if __name__ == "__main__":
     maps, seeds = parse_input("input.txt")
-    seeds = list(seeds)
-    new_seeds = []
 
+    # part 1
+    seed_ranges = [range(seed, seed + 1) for seed in seeds]
+    part_1 = ranges = get_locations_from_seed_ranges(maps, seed_ranges)
+    print(min(val.start for val in ranges))
+
+    # part 2
+    seed_ranges = []
     for ii in range(0, len(seeds), 2):
-        new_seeds += [range(seeds[ii], seeds[ii] + seeds[ii + 1])]
-    out = []
-    for seed_range in new_seeds:
-        for seed in seed_range:
-            mn = "seed-to-soil"
-            while mn:
-                current_map = maps[mn]
-                for map_range in current_map:
-                    if seed in map_range.source_range:
-                        value = seed - map_range.source_start
-                        seed = map_range.destination_start + value
-                        break
-                mn = MAP_NAMES[mn]
+        seed_ranges += [range(seeds[ii], seeds[ii] + seeds[ii + 1])]
 
-            out.append(seed)
-    print(min(out))
+    part_2 = get_locations_from_seed_ranges(maps, seed_ranges)
+    print(min(val.start for val in part_2))
